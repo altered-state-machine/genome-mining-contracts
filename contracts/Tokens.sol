d061f1622e51a0a211298b305ee0a6cbc97e679e
Alex.Tikonoff <alex.tikonoff@alteredstatemachine.xyz>
2022-05-25 13:21:23 +1200

    registry itself updatingadded

--- contracts/Tokens.sol
+++ contracts/Tokens.sol
@@ -4,6 +4,10 @@ pragma solidity ^0.8.6;
 
 import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
+import "./Staking.sol";
+import "./StakingStorage.sol";
+import "./Converter.sol";
+import "./ConverterStorage.sol";
 
 error WrongAddress(address addr, string errMsg);
 
@@ -22,11 +26,12 @@ error WrongAddress(address addr, string errMsg);
  */
 contract Registry is Ownable {
     address public multisig;
-    address public stakingLogicContract;
-    address public stakingStorageContract;
-    address public converterLogicContract;
-    address public converterStorageContract;
-    address public registryContract;
+
+    Registry public registryContract;
+    Staking public stakingLogicContract;
+    StakingStorage public stakingStorageContract;
+    Converter public converterLogicContract;
+    ConverterStorage public converterStorageContract;
 
     IERC20 immutable asto = IERC20(0x823556202e86763853b40e9cDE725f412e294689);
     IERC20 immutable lba = IERC20(0x823556202e86763853b40e9cDE725f412e294689);
@@ -50,17 +55,6 @@ contract Registry is Ownable {
         if (address(_multisig) == address(0)) {
             revert WrongAddress(_multisig, "Wrong multisig address");
         }
-        multisig = _multisig;
-        transferOwnership(_multisig);
-    }
-
-    function changeContracts(
-        address _stakingLogic,
-        address _stakingStorage,
-        address _converterLogic,
-        address _converterStorage,
-        address _registry
-    ) public onlyOwner {
         if (address(_stakingLogic) == address(0)) {
             revert WrongAddress(_stakingLogic, "Wrong staking logic address");
         }
@@ -82,14 +76,40 @@ contract Registry is Ownable {
                 "Wrong converter storage address"
             );
         }
-        if (address(_registry) == address(0)) {
-            revert WrongAddress(_registry, "Wrong registry address");
+
+        multisig = _multisig;
+        transferOwnership(_multisig);
+    }
+
+    function changeContracts(
+        address _stakingLogic,
+        address _stakingStorage,
+        address _converterLogic,
+        address _converterStorage,
+        address _registry
+    ) public onlyOwner {
+        if (address(_stakingLogic) != address(0)) {
+            stakingLogicContract = Staking(_stakingLogic);
+            // StakingStorage.updateManager(_stakingLogic);
         }
 
-        stakingLogicContract = _stakingLogic;
-        stakingStorageContract = _stakingStorage;
-        converterLogicContract = _converterLogic;
-        converterStorageContract = _converterStorage;
-        registryContract = _registry;
+        if (address(_stakingStorage) != address(0)) {
+            stakingStorageContract = StakingStorage(_stakingStorage);
+        }
+
+        if (address(_converterLogic) != address(0)) {
+            converterLogicContract = Converter(_converterLogic);
+            // StakingStorage.updateConverter(_converterLogic);
+            // ConverterStorage.updateManager(_converterLogic);
+        }
+        if (address(_converterStorage) != address(0)) {
+            converterStorageContract = ConverterStorage(_converterStorage);
+        }
+
+        if (address(_registry) != address(0)) {
+            registryContract = Registry(_registry);
+            // StakingStorage.updateRegistry(_registry);
+            // ConverterStorage.updateRegistry(_registry);
+        }
     }
 }
