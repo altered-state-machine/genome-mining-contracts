58c67ec2af2d0f5f134db386cf91d8001e749ade
Alex.Tikonoff <alex.tikonoff@alteredstatemachine.xyz>
2022-06-04 13:45:55 +1200

    test file updated

--- tests/EnergyStorage.test.sol
+++ tests/EnergyStorage.test.sol
@@ -15,10 +15,15 @@ import "forge-std/Vm.sol";
 contract EnergyStorageTestContract is DSTest, Util {
     EnergyStorage energyStorage_;
     Converter converterLogic_;
+    Controller controller_;
 
     address someone = 0xA847d497b38B9e11833EAc3ea03921B40e6d847c;
     address deployer = address(this);
     address multisig = deployer; // for the testing we use deployer as a multisig
+
+    // Cheat codes are state changing methods called from the address:
+    // 0x7109709ECfa91a80626fF3989D68f67F5b1DD12D
+    Vm vm = Vm(0x7109709ECfa91a80626fF3989D68f67F5b1DD12D);
 
     function setUp() public {
         setupContracts();
