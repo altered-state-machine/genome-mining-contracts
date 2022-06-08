0e8e999d4256e2aa1f64473d3eb0844e07b942b9
VeryCB <imcaibin@gmail.com>
2022-06-09 09:40:31 +1200

    update controller deployment script

--- deploy/07_init_controller.ts
+++ deploy/07_init_controller.ts
@@ -5,13 +5,12 @@ import { ethers } from "hardhat";
 const func: DeployFunction = async (hre: HardhatRuntimeEnvironment) => {
   const { getNamedAccounts, deployments } = hre;
   const { deploy, execute } = deployments;
-  const { deployer, agent } = await getNamedAccounts();
+  const { deployer } = await getNamedAccounts();
 
-
-  // const astoToken = await hre.deployments.get("ASTOToken");
-  // const lpToken = await hre.deployments.get("LPToken");
-  const astoTokenAddress = '0xaeF0327c6726f6B44b56Bc7036cD4710f632b52E';
-  const lpTokenAddress = '0xaeF0327c6726f6B44b56Bc7036cD4710f632b52E';
+  const astoToken = await hre.deployments.get("ASTOToken");
+  const lpToken = await hre.deployments.get("LPToken");
+  const astoTokenAddress = "0xaeF0327c6726f6B44b56Bc7036cD4710f632b52E";
+  const lpTokenAddress = "0xaeF0327c6726f6B44b56Bc7036cD4710f632b52E";
   const astoStorage = await hre.deployments.get("ASTOStorage");
   const lpStorage = await hre.deployments.get("LPStorage");
   const energyStorage = await hre.deployments.get("EnergyStorage");
@@ -19,58 +18,45 @@ const func: DeployFunction = async (hre: HardhatRuntimeEnvironment) => {
   const staking = await hre.deployments.get("Staking");
   const controller = await hre.deployments.get("Controller");
 
-
-
-  console.log({ deployer, agent}, "Controller:", controller.address);
-
+  console.log({ deployer }, "Controller:", controller.address);
 
   const res = await execute(
     "Controller",
     { from: deployer },
-    "getManager"
+    "init",
+    astoToken.address,
+    astoStorage.address,
+    lpToken.address,
+    lpStorage.address,
+    staking.address,
+    converter.address,
+    energyStorage.address
   );
 
-  // const res = await execute(
-  //   "Controller",
-  //   { from: deployer, gasLimit: 10000000 },
-  //   "init",
-  //     astoTokenAddress, 
-  //     astoStorage.address, 
-  //     lpTokenAddress, 
-  //     lpStorage.address, 
-  //     staking.address, 
-  //     converter.address, 
-  //     energyStorage.address
-  // );
-
-  // console.log("Adding period to ConverterTest contract...");
-  // const startTime = 1654560000; // June 7, 2022 00:00:00 UTC
-  // const endTime = 1659744000; // August 6, 2022 12:00:00 UTC
-  // const astoMultiplier = ethers.utils.parseEther("1");
-  // const lpMultiplier = ethers.utils.parseEther("1.36");
-  // await run("ConverterTest", { from: deployer }, "addPeriod", [
-  //   startTime.toString(),
-  //   endTime.toString(),
-  //   astoMultiplier,
-  //   lpMultiplier,
-  // ]);
-
-
-
-
-  // await energyConverter.addPeriods()
+  console.log("Adding period to ConverterTest contract...");
+  // update the timestamp and multipliers based on your requirements
+  const startTime = 1654560000; // June 7, 2022 00:00:00 UTC
+  const endTime = 1659744000; // August 6, 2022 12:00:00 UTC
+  const astoMultiplier = ethers.utils.parseEther("1");
+  const lpMultiplier = ethers.utils.parseEther("1.36");
+  await execute("Converter", { from: deployer }, "addPeriod", [
+    startTime.toString(),
+    endTime.toString(),
+    astoMultiplier,
+    lpMultiplier,
+  ]);
 };
 
 func.tags = ["ControllerInit"];
 
 func.dependencies = [
-  "ASTOStorage", 
-  "LPStorage", 
-  "Staking", 
-  "StakingStorage", 
-  "EnergyStorage", 
-  "Converter", 
-  "Controller"
+  "ASTOStorage",
+  "LPStorage",
+  "Staking",
+  "StakingStorage",
+  "EnergyStorage",
+  "Converter",
+  "Controller",
 ];
 
 export default func;
