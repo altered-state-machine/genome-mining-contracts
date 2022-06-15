fa360dc5a19ad70afd90762c9c09e7b6b3b2bd04
VeryCB <imcaibin@gmail.com>
2022-06-15 20:45:53 +1200

    Fix update role in PermissionControl

--- contracts/helpers/PermissionControl.sol
+++ contracts/helpers/PermissionControl.sol
@@ -2,7 +2,7 @@
 
 pragma solidity ^0.8.13;
 
-import "@openzeppelin/contracts/access/AccessControl.sol";
+import "@openzeppelin/contracts/access/AccessControlEnumerable.sol";
 
 /**
  * @dev ASM Genome Mining - PermissionControl contract
@@ -13,13 +13,14 @@ bytes32 constant MULTISIG_ROLE = keccak256("MULTISIG_ROLE");
 bytes32 constant DAO_ROLE = keccak256("DAO_ROLE");
 bytes32 constant CONSUMER_ROLE = keccak256("CONSUMER_ROLE");
 
-contract PermissionControl is AccessControl {
+contract PermissionControl is AccessControlEnumerable {
     /**
-     * @dev Update `role` from the sender to `_newAddress`.
+     * @dev Update `role` from the sender to `newAddress`.
      * @dev Internal function without access restriction.
      */
-    function _updateRole(bytes32 role, address _newAddress) internal {
-        _revokeRole(role, msg.sender);
-        _grantRole(role, _newAddress);
+    function _updateRole(bytes32 role, address newAddress) internal {
+        address oldAddress = getRoleMember(role, 0);
+        _revokeRole(role, oldAddress);
+        _grantRole(role, newAddress);
     }
 }
